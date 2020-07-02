efbb35d9537bad9edde0ac3ac3f0efbcf707020f
Dannus90 <persson.daniel.1990@gmail.com>
2020-07-03 01:43:37 +0200

    remove cors headers

--- server.js
+++ server.js
@@ -19,33 +19,6 @@ const postRoute = require("./routes/posts");
 app.use(cors());
 
 // //More cors avoidance
-// Add headers
-app.use(function (req, res, next) {
-    // Website you wish to allow to connect
-    res.setHeader(
-        "Access-Control-Allow-Origin",
-        "https://pf-website-api.herokuapp.com/"
-    );
-
-    // Request methods you wish to allow
-    res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-    );
-
-    // Request headers you wish to allow
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "X-Requested-With,content-type"
-    );
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader("Access-Control-Allow-Credentials", true);
-
-    // Pass to next layer of middleware
-    next();
-});
 
 //Route middleware
 app.use("/api/sendMail", contactRoute);
